4964f51a1956fc03d613b71d65c8dc20c1f31477
Augusto Madeira Splett <augusto.splett@gmail.com>
2021-01-01 09:25:22 -0300

    salvando o estado atual para continuar trabalhando em outro momento

--- assets/features/index.js
+++ assets/features/index.js
@@ -1,12 +1,25 @@
 const body = document.querySelector('.corpo');
 const criar = document.querySelector('.btn');
-const goalData = document.querySelector('.destino');
-const goalName = document.querySelector('.goalName')
+const goalData = new Date(document.querySelector('.destino'));
+var goalName = document.querySelector('.goalName');
+
+var now = new Date(); 
+
+function timeRemaning(dataAlvo, dataAtual){
+    return dataAlvo.getFullYear() - dataAtual.getFullYear();
+}
+
+//var datetime = "Last Sync: " + currentdate.getDate() + "/"
+//                + (currentdate.getMonth()+1)  + "/" 
+//                + currentdate.getFullYear() + " @ "  
+//                + currentdate.getHours() + ":"  
+//                + currentdate.getMinutes() + ":" 
+//                + currentdate.getSeconds();
+
 criar.addEventListener('click', element =>{
-    body.innerText = goalName.value + goalData.value
+    //body.innerText = goalName.value +goalData.value + datetime
+    var ano = goalData.getFullYear();
+    body.innerText = ano;
+    //body.innerText = timeRemaning(goalData.value ,now)
+
 })
-//function criarAA(){
-//    console.log('hello');
-//    console.log(body);
-//    body.innerText ="hello"
-//}
